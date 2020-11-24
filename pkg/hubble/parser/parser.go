2d53640891479a3910e4de0b63435369a6f0d9d2
Tobias Klauser <tklauser@distanz.ch>
2020-11-24 16:07:42 +0100

    hubble/parser: parse monitor API agent events into hubble API messages
    This commit adds support to translate monitor API agents events as
    described by pkg/monitor/api/AgentNotifyMessage into Hubble API
    api/v1/flow.AgentEvent messages consumable by Hubble clients. The
    monitorAPI.AgentNotify* types are translated into their corresponding
    flowpb.AgentEventType_* type.
    
    Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

--- pkg/hubble/parser/parser.go
+++ pkg/hubble/parser/parser.go
@@ -15,6 +15,8 @@
 package parser
 
 import (
+	"time"
+
 	pb "github.com/cilium/cilium/api/v1/flow"
 	v1 "github.com/cilium/cilium/pkg/hubble/api/v1"
 	observerTypes "github.com/cilium/cilium/pkg/hubble/observer/types"
@@ -26,6 +28,8 @@ import (
 	monitorAPI "github.com/cilium/cilium/pkg/monitor/api"
 	"github.com/cilium/cilium/pkg/proxy/accesslog"
 	"github.com/golang/protobuf/ptypes"
+	"google.golang.org/protobuf/types/known/timestamppb"
+	"google.golang.org/protobuf/types/known/wrapperspb"
 
 	"github.com/golang/protobuf/ptypes/wrappers"
 	"github.com/sirupsen/logrus"
@@ -75,6 +79,222 @@ func lostEventSourceToProto(source int) pb.LostEventSource {
 	}
 }
 
+func agentNotifyTimeNotificationToProto(typ pb.AgentEventType, msg monitorAPI.AgentNotifyMessage) *pb.AgentEvent {
+	var ts *timestamppb.Timestamp
+	if notification, ok := msg.Notification.(monitorAPI.TimeNotification); ok {
+		if goTime, err := time.Parse(time.RFC3339Nano, notification.Time); err == nil {
+			ts, _ = ptypes.TimestampProto(goTime)
+		}
+	}
+	return &pb.AgentEvent{
+		Type: typ,
+		Notification: &pb.AgentEvent_Start{
+			Start: &pb.TimeNotification{
+				Time: ts,
+			},
+		},
+	}
+}
+
+func agentNotifyPolicyNotificationToProto(typ pb.AgentEventType, msg monitorAPI.AgentNotifyMessage) *pb.AgentEvent {
+	var ruleCount int
+	var revision uint64
+	var labels []string
+	if n, ok := msg.Notification.(monitorAPI.PolicyUpdateNotification); ok {
+		ruleCount = n.RuleCount
+		labels = n.Labels
+		revision = n.Revision
+	}
+
+	return &pb.AgentEvent{
+		Type: typ,
+		Notification: &pb.AgentEvent_PolicyUpdate{
+			PolicyUpdate: &pb.PolicyUpdateNotification{
+				RuleCount: int64(ruleCount),
+				Labels:    labels,
+				Revision:  revision,
+			},
+		},
+	}
+}
+
+func agentNotifyEndpointRegenNotificationToProto(typ pb.AgentEventType, msg monitorAPI.AgentNotifyMessage) *pb.AgentEvent {
+	var id uint64
+	var labels []string
+	var e string
+	if n, ok := msg.Notification.(monitorAPI.EndpointRegenNotification); ok {
+		id = n.ID
+		labels = n.Labels
+		e = n.Error
+	}
+
+	return &pb.AgentEvent{
+		Type: typ,
+		Notification: &pb.AgentEvent_EndpointRegenerate{
+			EndpointRegenerate: &pb.EndpointRegenNotification{
+				Id:     id,
+				Labels: labels,
+				Error:  e,
+			},
+		},
+	}
+}
+
+func agentNotifyEndpointUpdateNotificationToProto(typ pb.AgentEventType, msg monitorAPI.AgentNotifyMessage) *pb.AgentEvent {
+	var id uint64
+	var labels []string
+	var e, podName, namespace string
+	if n, ok := msg.Notification.(monitorAPI.EndpointNotification); ok {
+		id = n.ID
+		labels = n.Labels
+		e = n.Error
+		podName = n.PodName
+		namespace = n.Namespace
+	}
+
+	return &pb.AgentEvent{
+		Type: typ,
+		Notification: &pb.AgentEvent_EndpointUpdate{
+			EndpointUpdate: &pb.EndpointUpdateNotification{
+				Id:        id,
+				Labels:    labels,
+				Error:     e,
+				PodName:   podName,
+				Namespace: namespace,
+			},
+		},
+	}
+}
+func agentNotifyIPCacheNotificationToProto(typ pb.AgentEventType, msg monitorAPI.AgentNotifyMessage) *pb.AgentEvent {
+	var cidr string
+	var identity uint32
+	var oldIdentity *wrapperspb.UInt32Value
+	var hostIPString, oldHostIPString string
+	var encryptKey uint8
+	var namespace, podName string
+	if n, ok := msg.Notification.(monitorAPI.IPCacheNotification); ok {
+		cidr = n.CIDR
+		identity = n.Identity
+		if n.OldIdentity != nil {
+			oldIdentity = &wrapperspb.UInt32Value{
+				Value: *n.OldIdentity,
+			}
+		}
+		if n.HostIP != nil {
+			hostIPString = n.HostIP.String()
+		}
+		if n.OldHostIP != nil {
+			oldHostIPString = n.OldHostIP.String()
+		}
+		encryptKey = n.EncryptKey
+		namespace = n.Namespace
+		podName = n.PodName
+	}
+	return &pb.AgentEvent{
+		Type: typ,
+		Notification: &pb.AgentEvent_IpcacheUpdate{
+			IpcacheUpdate: &pb.IPCacheNotification{
+				Cidr:        cidr,
+				Identity:    identity,
+				OldIdentity: oldIdentity,
+				HostIp:      hostIPString,
+				OldHostIp:   oldHostIPString,
+				EncryptKey:  uint32(encryptKey),
+				Namespace:   namespace,
+				PodName:     podName,
+			},
+		},
+	}
+}
+
+func agentNotifyServiceUpsertedToProto(typ pb.AgentEventType, msg monitorAPI.AgentNotifyMessage) *pb.AgentEvent {
+	var id uint32
+	var feAddr *pb.ServiceUpsertNotificationAddr
+	var beAddrs []*pb.ServiceUpsertNotificationAddr
+	var svcType, svcTrafficPolicy, svcName, svcNamespace string
+	if n, ok := msg.Notification.(monitorAPI.ServiceUpsertNotification); ok {
+		id = n.ID
+		feAddr = &pb.ServiceUpsertNotificationAddr{
+			Ip:   n.Frontend.IP.String(),
+			Port: uint32(n.Frontend.Port),
+		}
+		for _, be := range n.Backends {
+			var ipStr string
+			if be.IP != nil {
+				ipStr = be.IP.String()
+			}
+			beAddrs = append(beAddrs, &pb.ServiceUpsertNotificationAddr{
+				Ip:   ipStr,
+				Port: uint32(be.Port),
+			})
+		}
+		svcType = n.Type
+		svcTrafficPolicy = n.TrafficPolicy
+		svcName = n.Name
+		svcNamespace = n.Namespace
+	}
+	return &pb.AgentEvent{
+		Type: typ,
+		Notification: &pb.AgentEvent_ServiceUpserted{
+			ServiceUpserted: &pb.ServiceUpsertNotification{
+				Id:               id,
+				FrontendAddress:  feAddr,
+				BackendAddresses: beAddrs,
+				Type:             svcType,
+				TrafficPolicy:    svcTrafficPolicy,
+				Name:             svcName,
+				Namespace:        svcNamespace,
+			},
+		},
+	}
+}
+
+func agentNotifyServiceDeletedToProto(typ pb.AgentEventType, msg monitorAPI.AgentNotifyMessage) *pb.AgentEvent {
+	var id uint32
+	if n, ok := msg.Notification.(monitorAPI.ServiceDeleteNotification); ok {
+		id = n.ID
+	}
+	return &pb.AgentEvent{
+		Type: typ,
+		Notification: &pb.AgentEvent_ServiceDeleted{
+			ServiceDeleted: &pb.ServiceDeleteNotification{
+				Id: id,
+			},
+		},
+	}
+}
+
+func agentNotifyMessageToProto(msg monitorAPI.AgentNotifyMessage) *pb.AgentEvent {
+	switch msg.Type {
+	case monitorAPI.AgentNotifyStart:
+		return agentNotifyTimeNotificationToProto(pb.AgentEventType_START, msg)
+	case monitorAPI.AgentNotifyPolicyUpdated:
+		return agentNotifyPolicyNotificationToProto(pb.AgentEventType_POLICY_UPDATED, msg)
+	case monitorAPI.AgentNotifyPolicyDeleted:
+		return agentNotifyPolicyNotificationToProto(pb.AgentEventType_POLICY_DELETED, msg)
+	case monitorAPI.AgentNotifyEndpointRegenerateSuccess:
+		return agentNotifyEndpointRegenNotificationToProto(pb.AgentEventType_ENDPOINT_REGENERATE_SUCCESS, msg)
+	case monitorAPI.AgentNotifyEndpointRegenerateFail:
+		return agentNotifyEndpointRegenNotificationToProto(pb.AgentEventType_ENDPOINT_REGENERATE_FAIL, msg)
+	case monitorAPI.AgentNotifyEndpointCreated:
+		return agentNotifyEndpointUpdateNotificationToProto(pb.AgentEventType_ENDPOINT_CREATED, msg)
+	case monitorAPI.AgentNotifyEndpointDeleted:
+		return agentNotifyEndpointUpdateNotificationToProto(pb.AgentEventType_ENDPOINT_DELETED, msg)
+	case monitorAPI.AgentNotifyIPCacheUpserted:
+		return agentNotifyIPCacheNotificationToProto(pb.AgentEventType_IPCACHE_UPSERTED, msg)
+	case monitorAPI.AgentNotifyIPCacheDeleted:
+		return agentNotifyIPCacheNotificationToProto(pb.AgentEventType_IPCACHE_DELETED, msg)
+	case monitorAPI.AgentNotifyServiceUpserted:
+		return agentNotifyServiceUpsertedToProto(pb.AgentEventType_SERVICE_UPSERTED, msg)
+	case monitorAPI.AgentNotifyServiceDeleted:
+		return agentNotifyServiceDeletedToProto(pb.AgentEventType_SERVICE_DELETED, msg)
+	default:
+		return &pb.AgentEvent{
+			Type: pb.AgentEventType_UNSPECIFIED,
+		}
+	}
+}
+
 // Decode decodes a cilium monitor 'payload' and returns a v1.Event with
 // the Event field populated.
 func (p *Parser) Decode(monitorEvent *observerTypes.MonitorEvent) (*v1.Event, error) {
@@ -116,6 +336,13 @@ func (p *Parser) Decode(monitorEvent *observerTypes.MonitorEvent) (*v1.Event, er
 			flow.Time = ts
 			flow.NodeName = monitorEvent.NodeName
 			ev.Event = flow
+			return ev, nil
+		case monitorAPI.MessageTypeAgent:
+			agentNotifyMessage, ok := payload.Message.(monitorAPI.AgentNotifyMessage)
+			if !ok {
+				return nil, errors.ErrInvalidAgentMessageType
+			}
+			ev.Event = agentNotifyMessageToProto(agentNotifyMessage)
 			return ev, nil
 		default:
 			return nil, errors.ErrUnknownEventType
--- pkg/hubble/parser/parser_test.go
+++ pkg/hubble/parser/parser_test.go
@@ -17,7 +17,9 @@
 package parser
 
 import (
+	stderrors "errors"
 	"io/ioutil"
+	"net"
 	"testing"
 	"time"
 
@@ -28,7 +30,9 @@ import (
 	"github.com/cilium/cilium/pkg/hubble/testutils"
 	"github.com/cilium/cilium/pkg/monitor"
 	"github.com/cilium/cilium/pkg/monitor/api"
+	monitorAPI "github.com/cilium/cilium/pkg/monitor/api"
 	"github.com/cilium/cilium/pkg/proxy/accesslog"
+	"google.golang.org/protobuf/types/known/wrapperspb"
 
 	"github.com/golang/protobuf/ptypes"
 	"github.com/golang/protobuf/ptypes/wrappers"
@@ -128,4 +132,341 @@ func Test_EventType_RecordLost(t *testing.T) {
 			Source:        flowpb.LostEventSource_PERF_EVENT_RING_BUFFER,
 		},
 	}, ev)
+}
+
+type mockEndpoint struct {
+	ID        uint64
+	Labels    []string
+	PodName   string
+	Namespace string
+}
+
+func (e *mockEndpoint) GetID() uint64           { return e.ID }
+func (e *mockEndpoint) GetOpLabels() []string   { return e.Labels }
+func (e *mockEndpoint) GetK8sPodName() string   { return e.PodName }
+func (e *mockEndpoint) GetK8sNamespace() string { return e.Namespace }
+
+func TestDecodeAgentEvent(t *testing.T) {
+	p, err := New(log, nil, nil, nil, nil, nil)
+	assert.NoError(t, err)
+
+	ts := time.Now()
+	protoTimestamp, err := ptypes.TimestampProto(ts)
+	assert.NoError(t, err)
+
+	agentStartTS := ts.Add(-10 * time.Minute)
+	protoAgentStartTimestamp, err := ptypes.TimestampProto(agentStartTS)
+	assert.NoError(t, err)
+
+	mockEP := &mockEndpoint{
+		ID:        65535,
+		Labels:    []string{"custom=label", "label=another"},
+		PodName:   "devnull",
+		Namespace: "hubble",
+	}
+
+	oldID := uint32(511)
+
+	tt := []struct {
+		name string
+		msg  api.AgentNotifyMessage
+		ev   *v1.Event
+	}{
+		{
+			name: "empty AgentNotifyMessage",
+			msg:  api.AgentNotifyMessage{},
+			ev: &v1.Event{
+				Timestamp: protoTimestamp,
+				Event:     &flowpb.AgentEvent{},
+			},
+		},
+		{
+			name: "unspecified AgentNotifyMessage",
+			msg: api.AgentNotifyMessage{
+				Type: api.AgentNotifyUnspec,
+				Notification: struct {
+					foo int64
+					bar int32
+				}{
+					foo: 23,
+					bar: 42,
+				},
+			},
+			ev: &v1.Event{
+				Timestamp: protoTimestamp,
+				Event:     &flowpb.AgentEvent{},
+			},
+		},
+		{
+			name: "StartMessage",
+			msg:  api.StartMessage(agentStartTS),
+			ev: &v1.Event{
+				Timestamp: protoTimestamp,
+				Event: &flowpb.AgentEvent{
+					Type: flowpb.AgentEventType_START,
+					Notification: &flowpb.AgentEvent_Start{
+						Start: &flowpb.TimeNotification{
+							Time: protoAgentStartTimestamp,
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "PolicyUpdateMessage",
+			msg:  api.PolicyUpdateMessage(42, []string{"hubble=rocks", "cilium=too"}, 7),
+			ev: &v1.Event{
+				Timestamp: protoTimestamp,
+				Event: &flowpb.AgentEvent{
+					Type: flowpb.AgentEventType_POLICY_UPDATED,
+					Notification: &flowpb.AgentEvent_PolicyUpdate{
+						PolicyUpdate: &flowpb.PolicyUpdateNotification{
+							RuleCount: 42,
+							Labels:    []string{"hubble=rocks", "cilium=too"},
+							Revision:  7,
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "PolicyDeleteMessage",
+			msg:  api.PolicyDeleteMessage(23, []string{"foo=bar"}, 255),
+			ev: &v1.Event{
+				Timestamp: protoTimestamp,
+				Event: &flowpb.AgentEvent{
+					Type: flowpb.AgentEventType_POLICY_DELETED,
+					Notification: &flowpb.AgentEvent_PolicyUpdate{
+						PolicyUpdate: &flowpb.PolicyUpdateNotification{
+							RuleCount: 23,
+							Labels:    []string{"foo=bar"},
+							Revision:  255,
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "EndpointRegenMessage success",
+			msg:  api.EndpointRegenMessage(mockEP, nil),
+			ev: &v1.Event{
+				Timestamp: protoTimestamp,
+				Event: &flowpb.AgentEvent{
+					Type: flowpb.AgentEventType_ENDPOINT_REGENERATE_SUCCESS,
+					Notification: &flowpb.AgentEvent_EndpointRegenerate{
+						EndpointRegenerate: &flowpb.EndpointRegenNotification{
+							Id:     mockEP.GetID(),
+							Labels: mockEP.GetOpLabels(),
+							Error:  "",
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "EndpointRegenMessage fail",
+			msg:  api.EndpointRegenMessage(mockEP, stderrors.New("error regenerating endpoint")),
+			ev: &v1.Event{
+				Timestamp: protoTimestamp,
+				Event: &flowpb.AgentEvent{
+					Type: flowpb.AgentEventType_ENDPOINT_REGENERATE_FAIL,
+					Notification: &flowpb.AgentEvent_EndpointRegenerate{
+						EndpointRegenerate: &flowpb.EndpointRegenNotification{
+							Id:     mockEP.GetID(),
+							Labels: mockEP.GetOpLabels(),
+							Error:  "error regenerating endpoint",
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "EndpointCreateMessage",
+			msg:  api.EndpointCreateMessage(mockEP),
+			ev: &v1.Event{
+				Timestamp: protoTimestamp,
+				Event: &flowpb.AgentEvent{
+					Type: flowpb.AgentEventType_ENDPOINT_CREATED,
+					Notification: &flowpb.AgentEvent_EndpointUpdate{
+						EndpointUpdate: &flowpb.EndpointUpdateNotification{
+							Id:        mockEP.GetID(),
+							Labels:    mockEP.GetOpLabels(),
+							Error:     "",
+							PodName:   mockEP.GetK8sPodName(),
+							Namespace: mockEP.GetK8sNamespace(),
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "EndpointDeleteMessage",
+			msg:  api.EndpointDeleteMessage(mockEP),
+			ev: &v1.Event{
+				Timestamp: protoTimestamp,
+				Event: &flowpb.AgentEvent{
+					Type: flowpb.AgentEventType_ENDPOINT_DELETED,
+					Notification: &flowpb.AgentEvent_EndpointUpdate{
+						EndpointUpdate: &flowpb.EndpointUpdateNotification{
+							Id:        mockEP.GetID(),
+							Labels:    mockEP.GetOpLabels(),
+							Error:     "",
+							PodName:   mockEP.GetK8sPodName(),
+							Namespace: mockEP.GetK8sNamespace(),
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "IPCacheUpsertedMessage (insert)",
+			msg:  api.IPCacheUpsertedMessage("10.0.1.42/32", 1023, nil, net.ParseIP("10.1.5.4"), nil, 0xff, "default", "foobar"),
+			ev: &v1.Event{
+				Timestamp: protoTimestamp,
+				Event: &flowpb.AgentEvent{
+					Type: flowpb.AgentEventType_IPCACHE_UPSERTED,
+					Notification: &flowpb.AgentEvent_IpcacheUpdate{
+						IpcacheUpdate: &flowpb.IPCacheNotification{
+							Cidr:        "10.0.1.42/32",
+							Identity:    1023,
+							OldIdentity: nil,
+							HostIp:      "10.1.5.4",
+							OldHostIp:   "",
+							EncryptKey:  0xff,
+							Namespace:   "default",
+							PodName:     "foobar",
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "IPCacheUpsertedMessage (update)",
+			msg:  api.IPCacheUpsertedMessage("192.168.10.11/32", 1023, &oldID, net.ParseIP("10.1.5.4"), net.ParseIP("10.2.6.11"), 5, "hubble", "podmcpodface"),
+			ev: &v1.Event{
+				Timestamp: protoTimestamp,
+				Event: &flowpb.AgentEvent{
+					Type: flowpb.AgentEventType_IPCACHE_UPSERTED,
+					Notification: &flowpb.AgentEvent_IpcacheUpdate{
+						IpcacheUpdate: &flowpb.IPCacheNotification{
+							Cidr:     "192.168.10.11/32",
+							Identity: 1023,
+							OldIdentity: &wrapperspb.UInt32Value{
+								Value: oldID,
+							},
+							HostIp:     "10.1.5.4",
+							OldHostIp:  "10.2.6.11",
+							EncryptKey: 5,
+							Namespace:  "hubble",
+							PodName:    "podmcpodface",
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "IPCacheDeletedMessage",
+			msg:  api.IPCacheDeletedMessage("192.168.10.0/24", 6048, nil, net.ParseIP("10.1.5.4"), nil, 0, "", ""),
+			ev: &v1.Event{
+				Timestamp: protoTimestamp,
+				Event: &flowpb.AgentEvent{
+					Type: flowpb.AgentEventType_IPCACHE_DELETED,
+					Notification: &flowpb.AgentEvent_IpcacheUpdate{
+						IpcacheUpdate: &flowpb.IPCacheNotification{
+							Cidr:        "192.168.10.0/24",
+							Identity:    6048,
+							OldIdentity: nil,
+							HostIp:      "10.1.5.4",
+							OldHostIp:   "",
+							EncryptKey:  0,
+							Namespace:   "",
+							PodName:     "",
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "ServiceUpsertMessage",
+			msg: api.ServiceUpsertMessage(
+				214,
+				monitorAPI.ServiceUpsertNotificationAddr{
+					IP:   net.ParseIP("10.240.12.1"),
+					Port: 8080,
+				},
+				[]monitorAPI.ServiceUpsertNotificationAddr{
+					{
+						IP:   net.ParseIP("192.168.3.59"),
+						Port: 9099,
+					},
+					{
+						IP:   net.ParseIP("192.168.3.57"),
+						Port: 7077,
+					},
+				},
+				"ClusterIP",
+				"myTrafficPolicy",
+				"myService",
+				"myNamespace",
+			),
+			ev: &v1.Event{
+				Timestamp: protoTimestamp,
+				Event: &flowpb.AgentEvent{
+					Type: flowpb.AgentEventType_SERVICE_UPSERTED,
+					Notification: &flowpb.AgentEvent_ServiceUpserted{
+						ServiceUpserted: &flowpb.ServiceUpsertNotification{
+							Id: 214,
+							FrontendAddress: &flowpb.ServiceUpsertNotificationAddr{
+								Ip:   "10.240.12.1",
+								Port: 8080,
+							},
+							BackendAddresses: []*flowpb.ServiceUpsertNotificationAddr{
+								{
+									Ip:   "192.168.3.59",
+									Port: 9099,
+								},
+								{
+									Ip:   "192.168.3.57",
+									Port: 7077,
+								},
+							},
+							Type:          "ClusterIP",
+							TrafficPolicy: "myTrafficPolicy",
+							Name:          "myService",
+							Namespace:     "myNamespace",
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "ServiceDeleteMessage",
+			msg:  api.ServiceDeleteMessage(1048575),
+			ev: &v1.Event{
+				Timestamp: protoTimestamp,
+				Event: &flowpb.AgentEvent{
+					Type: flowpb.AgentEventType_SERVICE_DELETED,
+					Notification: &flowpb.AgentEvent_ServiceDeleted{
+						ServiceDeleted: &flowpb.ServiceDeleteNotification{
+							Id: 1048575,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			ev, err := p.Decode(&observerTypes.MonitorEvent{
+				Timestamp: ts,
+				Payload: &observerTypes.AgentEvent{
+					Type:    monitorAPI.MessageTypeAgent,
+					Message: tc.msg,
+				},
+			})
+			assert.NoError(t, err)
+			assert.Equal(t, tc.ev, ev)
+		})
+	}
 }
